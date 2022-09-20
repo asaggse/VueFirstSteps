d7f051135304072ad1799ea26ab4a05f6da4c7d1
Alessandro Saggese <asaggese@gmail.com>
2022-09-20 17:18:37 +0200

    Create example.vue

---
+++ example.vue
@@ -0,0 +1,36 @@
+<template>
+	<section>
+    
+  	<p v-if="likes > 0">
+    	this video has {{ likes }} 👍🏻
+  	</p>
+    
+    <p v-else>
+    	nobody likes it 😭
+  	</p>
+    
+    <button v-on:click="likeAndSubscribe()">
+      Like & Subscribe 👍🏻
+    </button>
+    
+  </section>
+</template>
+
+<script>
+export default {
+  
+  data() {
+    return {
+      likes: 0,
+    }
+  },
+  
+  methods: {
+    likeAndSubscribe() {
+      this.likes++;
+      alert('Thanks! Have some cake 🍰')
+    }
+  }
+  
+}
+</script>
